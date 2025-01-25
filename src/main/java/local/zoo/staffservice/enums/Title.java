fec0deb1ba460ca05062acc6b2a021b047c4c7ba
derekgygax <derekgygax@gmail.com>
2025-01-24 23:01:30 -0500

    added enum Title needed for Staff model

---
+++ src/main/java/local/zoo/staffservice/enums/Title.java
@@ -0,0 +1,13 @@
+package local.zoo.staffservice.enums;
+
+public enum Title {
+    VETERINARIAN,
+    ZOOKEEPER,
+    ADMINISTRATOR,
+    GUIDE,
+    MAINTENANCE,
+    CURATOR,
+    RESEARCHER,
+    SECURITY,
+    ATTENDANT
+}
